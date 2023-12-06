51f4f724af520238f0571e8fd3f51ea47e3be278
Amarpreet Singh <amarpreetsingh@c02gk7j0md6m.hsd1.ca.comcast.net>
2023-12-06 13:43:58 -0800

    Initial commit

---
+++ test/bedrock-api-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BedrockApiProject from '../lib/bedrock-api-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bedrock-api-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BedrockApiProject.BedrockApiProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
